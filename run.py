27db11b2a329e67ef654540a120af546cf2dccd6
Yash Mahesh Patkar <126961037+YashPatkar@users.noreply.github.com>
2024-10-29 15:12:38 +0530

    Update run.py

--- run.py
+++ run.py
@@ -607,4 +607,4 @@ if __name__ == "__main__":
   # This line initializes the database by creating all the tables defined by the SQLAlchemy models and ensures that all the tables are created before the application starts 
   db.create_all()
   # This line starts the Flask server
-  app.run(debug=True)
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
